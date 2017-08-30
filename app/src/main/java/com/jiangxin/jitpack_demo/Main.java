cd3f73ba4870efa6432f2a3604e302c9dcad4940
xianming666 <18176625850@163.com>
2017-08-30 15:06:40 +0800

    v1.0.1

---
+++ app/src/main/java/com/jiangxin/jitpack_demo/Main.java
@@ -0,0 +1,13 @@
+package com.jiangxin.jitpack_demo;
+
+import android.app.Activity;
+
+/**
+ * Created by LXM on 2017/8/30.
+ */
+
+public class Main extends Activity {
+    private void dd() {
+
+    }
+}
---
+++ library/src/main/java/com/jiangxin/library/TestLib.java
@@ -0,0 +1,9 @@
+package com.jiangxin.library;
+
+/**
+ * Created by LXM on 2017/8/30.
+ */
+
+public class TestLib {
+    public void test(){}
+}
